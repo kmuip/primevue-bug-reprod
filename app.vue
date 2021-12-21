8cf2ee839c4de1af0bd3be04d357bfb89e5bf9c7
Lukas Zbinden <lukas.zbinden@kmupartnergroup.ch>
2021-12-21 17:17:14 +0100

    init commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Password
+      class="w-full"
+      v-model="password"
+      placeholder="Passwort"
+      :feedback="false"
+      toggle-mask
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const password = ref("");
+</script>
